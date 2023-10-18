d3f8a05d8f58cacaeb2f4b7fee49fecc72c26f52
Francisco Zanartu <fzanartu@outlook.com>
2023-10-18 02:50:37 +0000

    Add Predict class

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.functional as F
 from transformers import AutoModelForSequenceClassification
 from peft import LoraConfig, get_peft_model, TaskType, prepare_model_for_kbit_training
 
@@ -38,3 +39,28 @@ class ClassificationModel:
     def send_to_device(self):
         self.model.to(self.device)
 
+class Predict:
+
+    def __init__(self, load_dir):
+        
+        self.load_dir = load_dir
+        self.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+    def load_model(self):
+        self.model = AutoModelForSequenceClassification.from_pretrained(self.load_dir).to(self.device)
+        self.model.eval()
+
+    def predict(self, dataloader):
+        y_pred = []
+        self.load_model()
+        with torch.no_grad():
+            for batch in dataloader:
+                batch = {k: v.to(self.device) for k, v in batch.items()}
+                outputs = self.model(**batch)
+                logits = outputs.logits
+                probs = F.softmax(logits, dim=-1)
+                predictions = torch.argmax(probs, dim=-1)
+
+                y_pred.extend(predictions.cpu().numpy())
+        
+        return y_pred
